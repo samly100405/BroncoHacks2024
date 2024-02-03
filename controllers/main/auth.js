b1348a44f67a81824864a19c9ac13bb55cabdffe
Sam Ly <sam@samly.me>
2024-02-02 20:00:07 -0800

    working login

--- controllers/main/auth.js
+++
@@ -1,5 +0,0 @@
-import { app } from "../../firebase";
-import { getAuth } from "firebase/auth"
-
-const auth = getAuth(app)
-
--- frontend/main/src/components/LoginPage.jsx
+++ frontend/main/src/components/LoginPage.jsx
@@ -1,3 +1,7 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../../../firebase"
+import { useState } from "react";
+
 export default function LoginPage() {
     return (
         <section className="hero is-fullheight">
@@ -9,29 +13,46 @@ export default function LoginPage() {
 }
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const login = (e) => {
+        e.preventDefault();
+        signInWithEmailAndPassword(auth, email, password)
+        .then( (userCredential) => {
+            console.log(userCredential)
+        })
+    }
+    
     return (
         <div className="container">
             <div className="columns is-centered">
                 <div className="column is-5-tablet is-4-desktop is-3-widescreen">
                     <h1 className="title">Student Login</h1>
-                    <form action="" className="box">
+                    <form onSubmit={login} className="box">
                         <div className="field">
                             <label htmlFor="" className="label">Email</label>
                             <div className="control">
-                                <input type="email" className="input" placeholder="bobsmith@example.com" required/>
+                                <input 
+                                    value={email} 
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    type="email" className="input" placeholder="bobsmith@example.com" required/>
                             </div>
                         </div>
 
                         <div className="field">
                             <label htmlFor="" className="label">Passsword</label>
                             <div className="control">
-                                <input type="password" placeholder="********" className="input" required/>
+                                <input 
+                                    value={password} 
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    type="password" placeholder="********" className="input" required/>
                             </div>
                         </div>
 
-                        <button className="button is-primary mr-5">Login</button>
+                        <button type="submit" className="button is-primary mr-5">Login</button>
 
-                        <button className="button is-secondary">Sign up</button>
+                        {/* <button className="button is-secondary">Sign up</button> */}
                     </form>
                 </div>
             </div>
